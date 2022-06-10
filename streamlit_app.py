d3446fb370e0db399356833df1f6ccb41fb5389d
Magnus N <magnus.nordlund@xlent.se>
2022-06-10 16:43:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3...')
+streamlit.text('Kale...')
+streamlit.text('Hard-boiled...')
+
