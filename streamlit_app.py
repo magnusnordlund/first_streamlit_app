3646a50d614e2a1d250b7bbec9822ffddad89cfe
Magnus N <magnus.nordlund@xlent.se>
2022-06-20 17:53:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,5 @@ streamlit.dataframe(fruits_to_show)
 
 #New section Lesson 9
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.header('Fruityvice Fruit Advice!')
 streamlit.text(fruityvice_response)
